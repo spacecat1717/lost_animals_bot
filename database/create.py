30435224ddeac60cb962d8abb58c9e4792e00af8
spacecat1717 <manyam245@gmail.com>
2023-01-04 12:56:28 +0300

    database: create done

--- database/create.py
+++ database/create.py
@@ -14,10 +14,10 @@ class CreateTables:
         command = (
             "CREATE TABLE IF NOT EXISTS users(\
             id SERIAL,\
-            tlg_id BIGINT,\
+            tlg_id BIGINT NOT NULL,\
             username VARCHAR(64),\
             is_admin BOOL,\
-            is_banned BOO;,\
+            is_banned BOOL,\
             CONSTRAINT unique_tlg_id UNIQUE (tlg_id)\
             )"
         )
@@ -30,4 +30,58 @@ class CreateTables:
             Log.logger.error('[DB] Could not create table users. Reason: ', e)
             return False
 
+    async def lost_animals(self) -> bool:
+        command = (
+            "CREATE TABLE IF NOT EXISTS lost_animals(\
+            id SERIAL,\
+            owner BIGINT,\
+            location TEXT,\
+            date DATE,\
+            photo1 TEXT,\
+            photo2 TEXT,\
+            photo3 TEXT,\
+            name VARCHAR(64) NOT NULL,\
+            species VARCHAR(16) NOT NULL,\
+            description TEXT,\
+            chip BOOL,\
+            found BOOL,\
+            found_reason VARCHAR(32)\
+            CONSTRAINT fk_owner_lost FOREIGN KEY(owner) REFERENCES users(id)\
+            )"
+        )
+        try:
+            async with self._connection as conn:
+                await conn.execute(command)
+            Log.logger.info('[DB] Table lost_animals has been created')
+            return True
+        except Exception as e:
+            Log.logger.error('[DB] Could not create table lost_animals. Reason:  ', e)
+            return False
 
+    async def found_animals(self) -> bool:
+        command = (
+            "CREATE TABLE IF NOT EXISTS found_animals(\
+                id SERIAL,\
+                owner BIGINT,\
+                location TEXT,\
+                date DATE,\
+                photo1 TEXT,\
+                photo2 TEXT,\
+                photo3 TEXT,\
+                name VARCHAR(64) NOT NULL,\
+                species VARCHAR(16) NOT NULL,\
+                description TEXT,\
+                chip BOOL,\
+                found BOOL,\
+                found_reason VARCHAR(32)\
+                CONSTRAINT fk_owner_found FOREIGN KEY(owner) REFERENCES users(id)\
+                )"
+        )
+        try:
+            async with self._connection as conn:
+                await conn.execute(command)
+            Log.logger.info('[DB] Table found_animals has been created')
+            return True
+        except Exception as e:
+            Log.logger.error('[DB] Could not create table found_animals. Reason: ', e)
+            return False

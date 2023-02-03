1e5cc0d8860068787eba7c1601f97d569df4dae1
spacecat1717 <manyam245@gmail.com>
2023-02-03 16:30:34 +0300

    wrote Geocoder class

--- locations/location_api.py
+++ locations/location_api.py
@@ -3,20 +3,21 @@ import httpx
 
 from config.config import MAP_TOKEN
 from config.log_config import Logger as Log
-from database.connection import Connection
+
 
 class Geocoder:
-    def __init__(self):
-        self._connection = Connection()
 
-    async def smth(self, location: str):
-        get_city_url = f'https://geocode-maps.yandex.ru/1.x?format=json&lang=ru_RU&kind=district&geocode={location}&apikey={MAP_TOKEN}'
+    async def get_location_data(self, location: str) -> tuple:
+        Log.logger.info('[GEOCODER]Making request to API')
+        url = f'https://geocode-maps.yandex.ru/1.x?format=json&lang=ru_RU&kind=district&geocode={location}&apikey={MAP_TOKEN}'
         async with httpx.AsyncClient(trust_env=True) as client:
-            response_city = await client.get(get_city_url)
-        print(response_city.json())
+            response = await client.get(url)
+        if response.status_code == 200:
+            city = response.json()["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["metaDataProperty"]["GeocoderMetaData"]["Address"]["Components"][4]["name"]
+            district = response.json()["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["metaDataProperty"]["GeocoderMetaData"]["Address"]["Components"][2]["name"]
+            Log.logger.info('[GEOCODER] Location data has been found')
+            return city, district
+        Log.logger.error("[GEOCODER] API returned code %r, data hasn't been found", response.status_code)
 
-
-g = Geocoder()
-asyncio.run(g.smth('59.918085,30.349826'))
 
 
